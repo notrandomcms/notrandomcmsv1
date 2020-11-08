bc757ed02ff3927ab7ce0298be1099a4ca81dbe0
notrandomcms <prod@notrandomcms.com>
2020-11-08 09:29:34 +0000

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'NotARandom CMS - Start Page';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You are about to use the best CMS available for you website!</p>
+    </div>
+
+    <div class="body-content">
+
+       
+
+    </div>
+</div>
